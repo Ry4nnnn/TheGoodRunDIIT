f63ccab64215ece17c8f871ae8cf3e41fbd7d378
Ryan <weijunlimm@gmail.com>
2025-02-04 18:18:54 +0800

    modification on registration summary

--- Group2_DIIT.cpp
+++ Group2_DIIT.cpp
@@ -2,7 +2,21 @@
 #include <string>
 #include <vector>
 #include <limits.h>
+#include <iomanip>
+
 using namespace	std;
+
+//----------COLOURS----------//
+/* Regular */
+# define RESET		"\033[0m"
+# define BLACK		"\033[0;30m"
+# define RED		"\033[0;31m"
+# define GREEN		"\033[0;32m"
+# define YELLOW		"\033[0;33m"
+# define BLUE		"\033[0;34m"
+# define MAGENTA	"\033[0;35m"
+# define CYAN		"\033[0;36m"
+# define WHITE		"\033[0;37m"
 
 //a struct/template to store each player's data
 struct player {
@@ -71,6 +85,7 @@ int main() {
 		cout << "Enter age: ";
         cin >> p.age;
         cout << "Enter contact number: ";
+		cin.ignore();
         getline(cin, p.contactNum);
 
 		displayPrice();
@@ -78,10 +93,17 @@ int main() {
 		cin >> type;
 		cout << "Enter fee type (early/normal): ";
 		cin >> feeType;
+		
+		//print registration summary
+		cout << GREEN << "Registration Successful" << RESET << endl;
+		cout << CYAN << "Name: " << RESET << p.name << endl;
+		cout << CYAN << "IC / Passport Number: " << RESET << p.idNum << endl;
+		cout << CYAN << "Age: " << RESET << p.age << endl;
+		cout << CYAN << "Contact Number: " << RESET << p.contactNum << endl;
+		cout << BLUE << "Do you want to add another participant? (y/n): " << RESET;
 
-		cout << "Do you want to add another participant? (y/n): ";
 		cin >> c;
-		if (toupper(c) != 'N' || toupper(c) != 'Y')
+		if (toupper(c) != 'n' || toupper(c) != 'y')
 			cout << "Error! Invalid Input" << endl;
 	}
 	return (0);
